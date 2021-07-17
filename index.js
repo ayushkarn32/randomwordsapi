54f7eeefc27b158ceb0c86602878cb5cc6a159d8
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2021-07-17 17:32:24 +0545

    added new endpoint

--- index.js
+++ index.js
@@ -59,4 +59,44 @@ app.get('/', (req, res) => {
           });
         })
 
+    app.get('/getwords/:numberofwords/length/:length', (req, res) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+        const words=[];
+        var numberofwords=req.params.numberofwords;
+        var wordlength=req.params.length;
+        fs.readFile("words.json", "utf8", (err, response) => {
+            if (err) {
+              console.log("Error reading file:", err);
+              return;
+            }
+            try {
+              const word = JSON.parse(response);
+              words.push(word)
+              const rateLimiter=()=>{
+                const sendwords=[{message:"success"},{words:[]}]
+                for(let i=0;i<numberofwords;i++){
+                  const collectWords = () =>{
+                    var temp=Math.floor(Math.random()*2466);
+                    if(words[0].word[temp].length==wordlength){
+                        sendwords[1].words.push(words[0].word[temp]) 
+                    }
+                    else if(wordlength<=2 || wordlength >=13){
+                        res.send({message:"no data available"})
+                    }
+                    else {
+                      collectWords();
+                    }
+                  }
+                  collectWords();
+                }
+                res.status(200).send(sendwords)
+              }
+              rateLimiter();
+            } catch (err) {
+              console.log("Error parsing JSON string:", err);
+            }
+          });
+        })
+
 app.listen(process.env.PORT||3000);
